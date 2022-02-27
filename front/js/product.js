8ff4b1f8b5ccf47861a09d60d59ef97405e3c034
matfreitasc <matfreitasc@gmail.com>
2022-02-27 17:34:16 -0500

    add product.js and initiated the fetch call

---
+++ front/js/product.js
@@ -0,0 +1,72 @@
+let currentItems = {
+  colors: "",
+  _id: "",
+  name: "",
+  price: "",
+  imageUrl: "",
+  description: "",
+  altTxt: "",
+  quantity: "",
+};
+
+const url = new URL("http://localhost:3000/api/products");
+
+const urlID = new URLSearchParams(window.location.search);
+const id = urlID.get("id");
+
+const dropDownColors = document.querySelector("#colors");
+
+fetch(url + "/" + id)
+  .then((response) => response.json())
+  .then((jsonResponse) => createItem(jsonResponse));
+
+function createItem(data) {
+  console.log(data);
+
+  // Change title of the page
+  document.querySelector("title").innerHTML = data.name;
+
+  //Product Image
+  document.querySelector("#img").innerHTML =
+    '<img src="' + data.imageUrl + '" alt="' + data.altTxt + '">';
+
+  //Name of the product
+  document.querySelector("#title").innerHTML = data.name;
+
+  //Price of the product
+  document.querySelector("#price").innerHTML = data.price;
+
+  //Product Description
+  document.querySelector("#description").innerHTML = data.description;
+
+  //Color Dropdown Menu
+  addDropDownColors(data.colors);
+  dropDownColors.addEventListener("change", (e) => {
+    data.colors = e.target.value;
+  });
+
+  //Add To Cart Button
+  document.querySelector("#addToCart").addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+
+  // Push quantity to the cart
+  getItemQuantity(data.value);
+}
+// Add colors to the dropdown menu
+function addDropDownColors(colors) {
+  colors.forEach((color) => {
+    let option = document.createElement("option");
+    option.value = color;
+    option.innerHTML = color;
+    dropDownColors.appendChild(option);
+  });
+}
+
+// Handle quanity of the product
+function getItemQuantity(value) {
+  let quantity = document.querySelector("#quantity");
+  quantity.addEventListener("change", (e) => {
+    createItem.quantity = e.target.value;
+  });
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,25 @@
-// Connection with the database is done in the following function 
+let items = document.querySelector("#items");
+console.log(items);
+// Connection with the database is done in the following function
 fetch("http://localhost:3000/api/products")
   .then((response) => response.json())
   .then((jsonResponse) => createProductCards(jsonResponse));
 
 function createProductCards(productCards) {
   console.log(productCards);
-  // Finish this function
+  productCards.forEach((productCard) => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+
+        <a href="./product.html?id=${productCard._id}">
+        <article>
+        <img src="${productCard.imageUrl}" alt="${productCard.altTxt}">
+        <h3 class="productName">${productCard.name}</h3>
+        <p class="productDescription">${productCard.description}</p>
+        </article>
+        </a>
+        `;
+    items.appendChild(card);
+  });
 }
