5ede7282f5f3f19a9970b73da13cb4acd7d17ded
Matheus Cristofoli <matfreitasc@gmail.com>
2022-03-01 07:43:23 -0500

    add handle item delete and handle item quantity, also added total price

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,41 +5,72 @@ let cart__items = document.querySelector("#cart__items");
 console.log(cart);
 
 function addToCart() {
-  cart.forEach((item) => {
+  for (i = 0; i < cart.length; i++) {
     let itemArticle = document.createElement("article");
     itemArticle.classList.add("cart__item");
-    itemArticle.setAttribute("data-id", cart._id);
-    itemArticle.setAttribute("data-colors", cart.colors);
+    itemArticle.setAttribute("data-id", cart[i]._id);
+    itemArticle.setAttribute("data-colors", cart[i].colors);
     cart__items.appendChild(itemArticle);
 
     itemArticle.innerHTML = `
-      <div class="cart__item__img">
-      <img src="${item.imageUrl}" alt="${item.altTxt}" />
-    </div>
-    <div class="cart__item__content">
-      <div class="cart__item__content__description">
-        <h2>${item.name}</h2>
-        <p>${item.colors}</p>
-        <p>€: ${item.price}</p>
-      </div>
-      <div class="cart__item__content__settings">
-        <div class="cart__item__content__settings__quantity">
-          <p>Qté :</p>
-          <input
-            type="number"
-            class="itemQuantity"
-            name="itemQuantity"
-            min="1"
-            max="100"
-            value="${item.quantity}"
-          />
+          <div class="cart__item__img">
+          <img src="${cart[i].imageUrl}" alt="${cart[i].altTxt}" />
         </div>
-        <div class="cart__item__content__settings__delete">
-          <p class="deleteItem">Delete</p>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${cart[i].name}</h2>
+            <p>${cart[i].colors}</p>
+            <p>€: ${cart[i].price}</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté :</p>
+              <input
+                type="number"
+                class="itemQuantity"
+                name="itemQuantity"
+                min="1"
+                max="100"
+                value="${cart[i].quantity}"
+              />
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Delete</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-      `;
-  });
+          `;
+  }
+  // handle item quantity change
+  let itemQuantity = document.querySelectorAll(".itemQuantity");
+  for (let i = 0; i < itemQuantity.length; i++) {
+    itemQuantity[i].addEventListener("input", (e) => {
+      cart[i].quantity = e.target.value;
+      // update cart in local storage
+      localStorage.setItem("cart", JSON.stringify(cart));
+      
+      console.log(cart);
+    });
+  }
+  // handle item delete
+  let deleteItem = document.querySelectorAll(".deleteItem");
+  for (let i = 0; i < deleteItem.length; i++) {
+    deleteItem[i].addEventListener("click", (e) => {
+      cart.splice(i, 1);
+      localStorage.setItem("cart", JSON.stringify(cart));
+      // delete item from cart
+      e.target.parentElement.parentElement.parentElement.parentElement.remove();
+
+      console.log(cart);
+    });
+  }
+  // get total price
+  let totalPrice = 0;
+  for (let i = 0; i < cart.length; i++) {
+    totalPrice += cart[i].price * cart[i].quantity;
+  }
+  document.querySelector("#totalPrice").innerHTML = totalPrice;
+
 }
+
 addToCart();
